3d75f613b4a662d5889e45aba8b2492c385b911e
TheEvandroLuis <theevandroluisaraujo@gmail.com>
2025-03-17 17:48:49 -0300

    Aula_17_03

---
+++ N1 e N2/C++/pandemia.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    int inf, r;
+    cin >> n >> m;
+    cin >> inf >> r;
+    int amigos[n] = {};
+
+
+
+    for (int i=0; i<r-1; i++ ){
+        int presentes;
+        cin >> presentes;
+        for (int j=0; j<presentes; j++){
+            int amigo;
+            cin >> amigo;
+        }
+    }
+
+
+
+    
+}
